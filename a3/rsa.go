96531a6f1608690601f19cc84791c4dcd8e4bd0a
Akanksha Vyas <vyasakanksha@gmail.com>
2011-11-20 13:30:19 -0500

    Written main and factorize functions
    main has the assignment of N and a call to factorize. Factorize
    takes a number as input and checks its factors starting at its
    square root. Does not work as square root function has not been
    written yet.

---
+++ a3/rsa.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+   "fmt"
+   "big"
+   )
+
+func main() {
+   N := new( big.Int )
+   N.SetString( "59862884897410580445294975930364810502194059460046784100782961089451413815651385722867179113058394170198337928137605175707021438787385809631292643776317258709958790233150162992821973384914482382094263768626624728641916686410126516762051754901449384230749023005608317630448024255295503829583920557675021174883", 10 )
+   fmt.Println( N )
+   factorize( N )
+}
+
+func factorize( n *big.Int )  {
+   factor := new( big.Int )
+   start := new( big.Int )
+   var end = big.NewInt(3)
+   fmt.Println( start )
+
+   i := start
+
+   for i.Cmp( end ) == 1 {
+      if i.Cmp( n ) == 0 {
+         factor = i
+         break;
+      }
+   }
+
+   fmt.Println( "la" )
+   fmt.Println( factor )
+}
+
